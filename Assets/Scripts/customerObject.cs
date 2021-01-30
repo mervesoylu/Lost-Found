560f5d064198664d6b5f7a2efe2653549c47445b
Sefanur <sefanursoyludesign@gmail.com>
2021-01-30 12:22:43 +1100

    customer instantiate

---
+++ Assets/Scripts/customerObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customerObject : MonoBehaviour
+{
+    public List<GameObject> objects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            Instantiate(objects[Random.Range(0, objects.Count)], transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
